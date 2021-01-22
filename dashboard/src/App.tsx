fc0c3222cdc12e3a720c340260c4bba79a6cd427
Dima Spodeniuk <dima.spodeniuk@gmail.com>
2021-01-22 10:46:05 +0200

    feat(dashboard): postMessage

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef } from 'react';
+
+import { MockData } from './data.mock';
+
 import './App.css';
 
 function App() {
+  const iframe = useRef<HTMLIFrameElement>(null);
+
+  const postData = () => {
+    iframe.current?.contentWindow?.postMessage(MockData, '*');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+      <h1>Dashboard</h1>
+      <iframe ref={iframe} name="preview" title="preview" src="http://localhost:3001" frameBorder={0} />
+      <button onClick={postData}>Post data</button>
+    </>
   );
 }
 
